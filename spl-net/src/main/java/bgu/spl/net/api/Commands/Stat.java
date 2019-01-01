13ae9f0eecc1862e0f51aa248a15850840224ef2
Shiri_V <shiriv1993@gmail.com>
2019-01-01 16:50:56 +0200

    Protocol v3

---
+++ spl-net/src/main/java/bgu/spl/net/api/Commands/Stat.java
@@ -0,0 +1,19 @@
+package bgu.spl.net.api.Commands;
+
+import bgu.spl.net.api.bidi.Command;
+
+public class Stat extends Command {
+
+    // Fields
+    private String userName;
+
+    // Public Constructor
+    public Stat(String userName) {
+        this.op_code = 8;
+        this.userName = userName;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+}
